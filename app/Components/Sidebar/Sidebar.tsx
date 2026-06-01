53556c617eb669b12bf34bb0119a433e0b68325b
Nathaniel D. Liebhart <nathaniel95975@gmail.com>
2026-06-01 09:06:53 -0500

    initial commit

---
+++ app/Components/Sidebar/Sidebar.tsx
@@ -0,0 +1,63 @@
+"use client";
+import React from "react";
+import styled from "styled-components";
+import { useGlobalState } from "@/app/context/globalProvider";
+import Image from "next/image";
+
+import menu from "@/app/utils/menu";
+import Link from "next/link";
+import { usePathname, useRouter } from "next/navigation";
+
+function Sidebar() {
+  const { theme } = useGlobalState();
+
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const handleClick = (link: string) => {
+    router.push(link);
+  };
+
+  console.log(theme);
+
+  return (
+    <SidebarStyled theme={theme}>
+      <div className='profile'>
+        <div className='profile-overlay'></div>
+        <div className='image'>
+          <Image width={70} height={70} src='/avatar1.png' alt='profile' />
+        </div>
+        <h1>
+          <span>Jane</span>
+          <span>Doe</span>
+        </h1>
+      </div>
+      <ul className='nav-items'>
+        {menu.map((item) => {
+          const link = item.link;
+          return (
+            <li
+              className={`nav-item ${pathname === link ? "active" : ""}`}
+              onClick={() => {
+                handleClick(link);
+              }}
+            >
+              {item.icon}
+              <Link href={link}>{item.title}</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </SidebarStyled>
+  );
+}
+
+const SidebarStyled = styled.nav`
+  position: relative;
+  width: ${(props) => props.theme.sidebarWidth};
+  background-color: ${(props) => props.theme.colorBg2};
+  border: 2px solid ${(props) => props.theme.borderColor2};
+  border-radius: 1rem;
+`;
+
+export default Sidebar;
